26e51829e187d7358b77e7492d00427d5160e3d8
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2023-11-27 00:58:38 -0500

    Correcion de los package

--- Juez Cachimbo/src/Interfaces/ProfesorUI/InterfazPanelesProfesor/Grupo.java
+++ Juez Cachimbo/src/Interfaces/ProfesorUI/InterfazPanelesProfesor/Grupo.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
-package InterfazPanelesProfesor;
+package Interfaces.ProfesorUI.InterfazPanelesProfesor;
 
 import java.awt.BorderLayout;
 import java.util.ArrayList;
